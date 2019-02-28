157f558605eb5c56104df4e68753e2ff1e9d9b5f
iam13islucky <4074135+iam13islucky@users.noreply.github.com>
2019-02-28 01:30:09 -0700

    Test scraping

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const puppeteer = require('puppeteer')
 let scrape = async () => {
 	const browser = await puppeteer.launch()
 	const page = await browser.newPage()
+	await page.goto(
+		'https://www.sourcewell-mn.gov/cooperative-purchasing/022217-wex'
+	)
+	const sourcewell = await page.evaluate(() => {
+		return document.querySelector(
+			'.vendor-contract-header__content > p.lead'
+		).innerText
+	})
 	await browser.close()
-	return 'Success!'
+	return sourcewell
 }
 
 scrape().then((value) => {
